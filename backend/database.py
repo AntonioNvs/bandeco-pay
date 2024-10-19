eef2a79ec7e878850aeebe8d54a1a7691b7ff919
rapha1201 <rapha1201@pc-noter>
2024-10-19 16:43:50 -0300

    database architeture, update on requirements and database archive

---
+++ backend/database.py
@@ -0,0 +1,131 @@
+import sqlite3
+
+# define connection and cursor
+
+connection = sqlite3.connect('bandecopay.db')
+
+cursor = connection.cursor()
+
+# adding tables on db
+
+user_table_command = """CREATE TABLE IF NOT EXISTS
+User(
+    user_id INTEGER PRIMARY KEY, 
+    name TEXT, 
+    balance FLOAT, 
+    price_to_pay FLOAT
+    );
+"""
+
+cursor.execute(user_table_command)#USER
+
+student_table_command = """CREATE TABLE IF NOT EXISTS
+Student(
+    registration_number INTEGER PRIMARY KEY,  
+    fump_level INTEGER,
+    user_id INTEGER,
+    FOREIGN KEY(user_id) REFERENCES User(user_id)
+    );
+"""
+
+cursor.execute(student_table_command)#STUDENT
+
+employee_table_command = """CREATE TABLE IF NOT EXISTS
+Employee(
+    employee_id INTEGER PRIMARY KEY, 
+    user_id INTEGER,
+    FOREIGN KEY(user_id) REFERENCES User(user_id)
+    );
+"""
+
+cursor.execute(employee_table_command)#EMPLOYEE
+
+teacher_table_command = """CREATE TABLE IF NOT EXISTS
+Teacher(
+    teacher_id INTEGER PRIMARY KEY, 
+    user_id INTEGER,
+    FOREIGN KEY(user_id) REFERENCES User(user_id)
+    );
+"""
+
+cursor.execute(teacher_table_command)#TEACHER
+
+card_table_command = """CREATE TABLE IF NOT EXISTS
+Card(
+    card_id TEXT PRIMARY KEY,
+    user_id INTEGER,
+    FOREIGN KEY(user_id) REFERENCES User(user_id)
+    );
+"""
+
+cursor.execute(card_table_command) #CARD
+
+restaurant_table_command = """CREATE TABLE IF NOT EXISTS
+Restaurant(
+    restaurant_id INTEGER PRIMARY KEY,
+    restaurant_name TEXT
+    );
+"""
+
+cursor.execute(restaurant_table_command)#RESTAURANT
+
+menu_table_command = """CREATE TABLE IF NOT EXISTS
+Menu(
+    menu_description TEXT,
+    day DATE,
+    day_period TEXT,
+    restaurant_id INTEGER,
+    FOREIGN KEY(restaurant_id) REFERENCES Restaurant(restaurant_id)
+    );
+"""
+
+cursor.execute(menu_table_command)
+
+transaction_table_command = """CREATE TABLE IF NOT EXISTS
+Traction(
+    transaction_id INTEGER PRIMARY KEY,
+    transaction_type TEXT,
+    transaction_value FLOAT,
+    transaction_date DATE,
+    restaurant TEXT,
+    user_id INTEGER,
+    card_id TEXT,
+    FOREIGN KEY(user_id) REFERENCES User(user_id),
+    FOREIGN KEY(card_id) REFERENCES Card(card_id)
+    );
+"""
+
+cursor.execute(transaction_table_command)
+
+# insert examples
+
+cursor.execute("""
+INSERT INTO User
+VALUES (123, "Antonio Caetano", 201.50, 6.50)
+""")
+
+cursor.execute("""
+INSERT INTO Student 
+VALUES (2022043213, 0, 123)
+""")
+
+cursor.execute("""
+INSERT INTO Card
+VALUES ("CD01_043", 123)
+""")
+
+cursor.execute("""
+INSERT INTO Traction
+VALUES (1, "pagamento_RU", 6.50, "RU01", 123, "CD01_043")
+""")
+
+# get tables function
+
+def print_tables(table_name):
+    cursor.execute(f"SELECT * FROM {table_name}")
+    print(cursor.fetchall())
+
+print_tables("User")
+print_tables("Student")
+print_tables("Card")
+print_tables("Traction")
